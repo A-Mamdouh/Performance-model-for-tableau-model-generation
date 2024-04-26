b8122689c6640714aa138e1bf77922a26e528753
Ahmed Mamdouh <medoduh@gmail.com>
2024-04-26 17:44:28 +0200

    heuristic structure update

--- src/heuristic.py
+++ src/heuristic.py
@@ -1,7 +1,27 @@
 from . import syntax
-from . import tableau
+from dataclasses import dataclass
+from typing import Iterable, Any, Tuple
+
+
+ContextObject = Any
+
+
+@dataclass
+class EventEmbedding:
+    event: syntax.Term
+    event_literals: Iterable[syntax.Formula]
 
 
 class Heuristic:
-    def scoreFormula(self, formula: syntax.Formula, tableau: tableau.Tableau) -> float:
-        return 0.0
+
+    def score_branch(
+        self, previous_context, event_embeddings: Iterable[EventEmbedding]
+    ) -> Tuple[ContextObject, float]:
+        event_embeddings_strs = [
+            (str(e.event), [str(x) for x in e.event_literals])
+            for e in event_embeddings
+        ]
+        return None, 0.0
+
+    def get_empty_context(self) -> ContextObject:
+        return None
