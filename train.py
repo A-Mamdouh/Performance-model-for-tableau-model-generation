a417aaf1866c647a9ba96219a5329af8eb0c8012
Ahmed Mamdouh <medoduh@gmail.com>
2024-06-02 15:26:09 +0200

    script to train a simple model

---
+++ train.py
@@ -0,0 +1,123 @@
+# %%
+from typing import List
+
+import matplotlib.pyplot as plt
+import torch
+import tqdm
+
+from src.heuristics.learned_heuristics.deep_learning_models.simple_lstm_model import LSTMModel, LSTMModelConfig
+from src.heuristics.min_events import MinEvents
+from src import narration
+from src.search.informed_agents import GreedyAgent
+from src.search.search_node import HeuristicTableauSearchNode
+from src.logic.syntax import Formula
+
+# %%
+def make_narrator() -> narration.Narrator:
+    run = narration.Verb("run", "ran")
+    sleep = narration.Verb("sleep", "slept")
+    eat = narration.Verb("eat", "ate")
+    john = "john"
+    bob = "bob"
+    mary = "mary"
+    story = [
+        narration.NounVerbSentence(john, eat),
+        narration.NounVerbSentence(bob, eat),
+        narration.NounNotVerbSentence(bob, run),
+        narration.NounAlwaysVerbSentence(bob, sleep),
+        narration.NounNotVerbSentence(mary, run),
+        narration.NounVerbSentence(bob, eat),
+    ]
+    return narration.Narrator(story)
+narrator = make_narrator()
+
+# %%
+def get_target_models(agent: GreedyAgent, narrator: narration.Narrator) -> List[HeuristicTableauSearchNode]:
+    return list(agent.search(narrator.copy()))
+
+models = get_target_models(GreedyAgent(MinEvents()), narrator)
+
+# %%
+def get_training_sequences(agent: GreedyAgent, solved_tree: List[HeuristicTableauSearchNode], device: str | torch.device) -> tuple[List[torch.Tensor], List[torch.Tensor]]:
+    # First, collect the model leaves
+    leaves: list[HeuristicTableauSearchNode] = [*solved_tree]
+    for node in solved_tree:
+        if node.parent in leaves:
+            leaves.remove(node.parent)
+    # Collect sequences
+    sequences: List[List[HeuristicTableauSearchNode]] = []
+    labels: List[List[float]] = []
+    for leaf in leaves:
+        sequence: List[HeuristicTableauSearchNode] = []
+        sequence_labels: List[float] = []
+        current: HeuristicTableauSearchNode = leaf
+        while current:
+            sequence.append(LSTMModel.get_node_encoding(current))
+            sequence_labels.append(current.priority)
+            current = current.parent
+        sequence = torch.concat(sequence[::-1], dim=0)
+        sequences.append(sequence)
+        sequence_labels = torch.tensor(sequence_labels, dtype=torch.float32, device=device)[:, None]
+        labels.append(sequence_labels)
+    return sequences, labels
+lstm_model = LSTMModel(LSTMModelConfig(
+    latent_size = 64,
+    hidden_size = 128,
+    num_layers=3,
+    dropout=0.5,
+    bidirectional=False
+))
+lstm_agent = GreedyAgent(heuristic=lstm_model)
+train_sequences, train_labels = get_training_sequences(lstm_agent, models, lstm_model._cfg.device)
+
+# %%
+def train(model: LSTMModel, train_sequences, train_labels, iters: int, lr: float):
+    loss_fn = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    loss_history = []
+    flat_labels = torch.stack(train_labels, dim=0)
+    iterator = tqdm.trange(iters, desc="training")
+    for _ in iterator:
+        optimizer.zero_grad()
+        out = model.forward_batch(train_sequences)[0]
+        flat_output = torch.stack(out, dim=0)
+        loss = loss_fn(flat_output, flat_labels)
+        loss.backward()
+        optimizer.step()
+        loss_value = loss.item()
+        iterator.set_description_str(f"Training loss: {loss_value:.4f}")
+        loss_history.append(loss_value)
+    return loss_history
+loss_history = train(lstm_model, train_sequences, train_labels, 20, lr=1e-5)
+plt.plot(loss_history)
+plt.show()
+
+# %%
+lstm_model.eval()
+with torch.inference_mode():
+    trained_models = get_target_models(lstm_agent, narrator)
+
+# %%
+with open("training_output", "w") as fp:
+    for model in trained_models:
+        # print("model:", *model.get_model(), sep=" ", end="\n")
+        print(
+            f"model @ {model.sentence_depth} - {model.priority[0,0]:.6f}: ",
+            *(
+                Formula.__str__(x)
+                for x in reversed(list(model.tableau.branch_formulas))
+                if x.annotation
+            ),
+            sep="\n ",
+            end="\n\n",
+            file=fp,
+        )
+        print(
+            "Entities:",
+            *(str(x) for x in reversed(list(model.tableau.branch_entities))),
+            sep="\n ",
+            file=fp,
+        )
+        print("-" * 30, file=fp)
+
+
