d1a5c72c1be308128ca1a06c8a9eaf1ec0acf464
Ahmed Mamdouh <medoduh@gmail.com>
2024-06-02 15:25:23 +0200

    refactor, network architecture update and batch forward

--- src/heuristics/learned_heuristics/deep_learning_models/simple_lstm_model.py
+++ src/heuristics/learned_heuristics/deep_learning_models/simple_lstm_model.py
@@ -7,6 +7,8 @@ from typing import List, Optional, Tuple
 
 import torch
 
+from src.heuristics.base_heuristic import Heuristic
+from src.heuristics.context_token import ContextToken
 from src.logic.tableau import EventInformation
 from src.search.search_node import TableauSearchNode
 
@@ -56,7 +58,7 @@ class InputData:
 
 
 @dataclass
-class ModelConfig:
+class LSTMModelConfig:
     """Configuration for DL model"""
 
     input_size: int = 3
@@ -81,19 +83,25 @@ class ModelConfig:
                 self.accelerated = False
 
 
-class Model(torch.nn.Module):
+class LSTMModel(torch.nn.Module, Heuristic):
     """Deep learning model.
     This model uses a linear feature extraction block,
     followed by an LSTM block, then a prediction head for the score output
     """
 
-    def __init__(self, cfg: Optional[ModelConfig] = None):
+    def __init__(self, cfg: Optional[LSTMModelConfig] = None):
         super().__init__()
         if cfg is None:
-            cfg = ModelConfig()
+            cfg = LSTMModelConfig()
         self._cfg = cfg
         self.features = torch.nn.Sequential(
             torch.nn.Linear(cfg.input_size, cfg.latent_size),
+            torch.nn.ReLU(),
+            torch.nn.Dropout1d(cfg.dropout),
+            torch.nn.Linear(cfg.latent_size, cfg.latent_size),
+            torch.nn.ReLU(),
+            torch.nn.Dropout1d(cfg.dropout),
+            torch.nn.Linear(cfg.latent_size, cfg.latent_size),
             torch.nn.ReLU(),
             torch.nn.Dropout1d(cfg.dropout),
         )
@@ -105,25 +113,31 @@ class Model(torch.nn.Module):
             bidirectional=cfg.bidirectional,
         )
         self.prediction_head = torch.nn.Sequential(
-            torch.nn.Linear(cfg.hidden_size, cfg.output_size),
-            torch.nn.ReLU(),
+            torch.nn.Linear(cfg.hidden_size, cfg.hidden_size),
+            torch.nn.Linear(cfg.hidden_size, cfg.output_size)
         )
         self._module_list = torch.nn.ModuleList(
             (self.features, self.memory_unit, self.prediction_head)
         )
         self.to(self._cfg.device)
+    
+    @staticmethod
+    def _make_empty_branch_tensor(input_: InputData) -> torch.Tensor:
+        """Create an input tensor that represents a model with no new event information."""
+        return torch.tensor(((input_.model_depth, 0, 0),), dtype=torch.float32)
 
-    def from_search_node(
+    def _from_search_node(
         self, node: TableauSearchNode, context: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """Return new score and context tuple from search node and previous context.
         This acts as a blackbox API for the heuristic module and the rest of the project
         """
-        input_tensor = InputData.from_search_node(node).get_encoding_tensor()
-        # If nothing new is in this node, return a copy of the input
-        if len(input_tensor) == 0:
-            return node.parent.priority, context.clone()
-        return self.forward(input_tensor, context)
+        input_data: InputData = InputData.from_search_node(node)
+        encoding: torch.Tensor = input_data.get_encoding_tensor()
+        # If nothing new is in this node, get the empty representaiton
+        if len(encoding) == 0:
+            encoding = self._make_empty_branch_tensor(input_data)
+        return self.forward(encoding, context)
 
     def forward(
         self, x: torch.Tensor, h: torch.Tensor
@@ -135,10 +149,37 @@ class Model(torch.nn.Module):
         y, h_new = self.memory_unit(features, h)
         score = self.prediction_head(y)
         return score, h_new
+    
+    def forward_batch(self, xs: List[torch.Tensor], h0s: torch.Tensor | None = None):
+        """ X is a list of sequences, h0s is a tensor with the starting hidden state for each sequence.
+        h0s should have the shape (Number of sequences) x (hidden layers) x (hidden state size),
+        regardless of the internal batch size first option.
+        return the output for each part of the sequences and the hidden states at the end of each sequence.
+        The output shapes matches the input shapes
+        """
+        if h0s is None:
+            h0s = torch.stack((self.get_empty_context(),) * len(xs), dim=0)
+        h0s = h0s.moveaxis(0, 1).to(self._cfg.device)
+        # First, get the lstm features input from the linear units
+        flat_sequences = torch.concat(xs, dim=0).to(self._cfg.device)
+        flat_features: torch.Tensor = self.features(flat_sequences) # Batch Size * Sequence Length x LSTM Input Shape
+        # Then, reshape back into the lstm input Length x BatchSize x latent_size and get the lstm output
+        reshaped_lstm_input = flat_features.split_with_sizes(list(map(len, xs)))
+        packed_feature_tensor = torch.nn.utils.rnn.pack_sequence(reshaped_lstm_input)
+        lstm_output_features, lstm_hidden_output = self.memory_unit(packed_feature_tensor, h0s)
+        # flatten the lstm output features to pass onto the prediction head
+        unpacked_lstm_features = torch.nn.utils.rnn.unpack_sequence(lstm_output_features)
+        flattened_lstm_features = torch.concat(unpacked_lstm_features, dim=0)
+        prediction_output = self.prediction_head(flattened_lstm_features)
+        # Reshape the prediction head output to match the input shape
+        output_predictions = prediction_output.split_with_sizes(list(map(len, xs)))
+        # Reshape the lstm hidden state outputo the first dimension is the batch
+        output_hidden_state_batch_first = lstm_hidden_output.moveaxis(1, 0)
+        return output_predictions, output_hidden_state_batch_first        
 
-    def get_initial_h(self) -> torch.Tensor:
+    def _get_initial_h(self) -> torch.Tensor:
         """Return a tensor for the initial history"""
-        cfg: ModelConfig = self._cfg
+        cfg: LSTMModelConfig = self._cfg
         d = 2 if cfg.bidirectional else 1
         return torch.zeros(
             (
@@ -148,3 +189,22 @@ class Model(torch.nn.Module):
             dtype=torch.float32,
             device=cfg.device,
         )
+    
+    def score_node(
+        self, previous_context, search_node: TableauSearchNode
+    ) -> Tuple[ContextToken, float]:
+        score, new_context = self._from_search_node(search_node, previous_context)
+        return new_context, score
+
+    def get_empty_context(self) -> ContextToken:
+        return self._get_initial_h()
+    
+    @staticmethod
+    def get_node_encoding(node: TableauSearchNode) -> torch.Tensor:
+        """Get the branch encoding tensor. Used for training"""
+        input_data: InputData = InputData.from_search_node(node)
+        encoding: torch.Tensor = input_data.get_encoding_tensor()
+        # If nothing new is in this node, get the empty representaiton
+        if len(encoding) == 0:
+            encoding = LSTMModel._make_empty_branch_tensor(input_data)
+        return encoding
