481d797c3525e8cb97f745b24d290a77a058f055
Ahmed Mamdouh <medoduh@gmail.com>
2024-06-02 15:25:42 +0200

    min events now has penality and reward instead of a mean

--- src/heuristics/min_events.py
+++ src/heuristics/min_events.py
@@ -12,10 +12,14 @@ from src.search.search_node import TableauSearchNode
 class MinEvents(Heuristic):
     """This heuristic prefers models with less entities"""
 
+    def __init__(self, event_penalty: float = -20, model_depth_reward: float = 100):
+        self.event_penalty = event_penalty
+        self.model_depth_reward = model_depth_reward
+
     def score_node(
         self, previous_context, search_node: TableauSearchNode
     ) -> Tuple[ContextToken, float]:
         context = super().score_node(previous_context, search_node)[0]
         # Set the score to be the number of events / sentence depth
-        n_events = len(list(search_node.tableau.events))
-        return context, n_events / max(1, search_node.sentence_depth)
+        n_events = len(list(search_node.tableau.branch_events))
+        return context, -(n_events * self.event_penalty + search_node.sentence_depth * self.model_depth_reward)
